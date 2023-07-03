e1c6c9704e10d4fbea1a04371ea098f03132a1fa
Redstonia <redstonia1234@gmail.com>
2023-07-03 13:48:13 +1200

    config template for this mod

--- src/main/java/screenshot_to_clipboard/ScreenshotToClipboardMod.java
+++ src/main/java/screenshot_to_clipboard/ScreenshotToClipboardMod.java
@@ -1,11 +1,12 @@
-package template;
+package screenshot_to_clipboard;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TemplateMod implements ModInitializer {
-	public static final String MOD_ID = "";
+public class ScreenshotToClipboardMod
+		implements ModInitializer {
+	public static final String MOD_ID = "screenshot-to-clipboard";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
