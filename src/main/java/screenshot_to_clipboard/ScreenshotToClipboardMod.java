fa9c66d45a91e54024d306f0fba6bd4fafff9680
Redstonia <redstonia1234@gmail.com>
2023-07-05 09:52:46 +1200

    quick changes and text feedback

--- src/main/java/screenshot_to_clipboard/ScreenshotToClipboardMod.java
+++ src/main/java/screenshot_to_clipboard/ScreenshotToClipboardMod.java
@@ -1,34 +1,46 @@
 package screenshot_to_clipboard;
 
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import screenshot_to_clipboard.util.ClipboardUtil;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.function.Consumer;
 
 public class ScreenshotToClipboardMod {
 	public static final String MOD_ID = "screenshot-to-clipboard";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	public static File screenshotFile;
+	public static Consumer<Text> messageReceiver;
 
 	public static void onInitialize() {
 		// cause clipboard stuff won't work with headless
 		System.setProperty("java.awt.headless", "false");
 	}
 
-	public static void holdScreenshotFile(File file) {
+	public static void captureRelevantLocals(File file, Consumer<Text> receiver) {
 		screenshotFile = file;
+		messageReceiver = receiver;
 	}
 
 	public static void sendHeldImageToClipboard() {
 		try {
-			BufferedImage image = ImageIO.read(screenshotFile);
-			ClipboardUtil.copyToClipboard(image);
+			if (!GraphicsEnvironment.isHeadless()) {
+				BufferedImage image = ImageIO.read(screenshotFile);
+				ClipboardUtil.copyToClipboard(image);
+				messageReceiver.accept(Text.literal("and copied to clipboard!").formatted(Formatting.GREEN));
+			} else {
+				messageReceiver.accept(Text.literal("Lost my head somewhere, please give it back!").formatted(Formatting.RED));
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
+			messageReceiver.accept(Text.literal("Failed to copy screenshot to clipboard.").formatted(Formatting.RED));
 		}
 	}
 }
--- src/main/java/screenshot_to_clipboard/mixin/ScreenshotRecorderMixin.java
+++ src/main/java/screenshot_to_clipboard/mixin/ScreenshotRecorderMixin.java
@@ -21,14 +21,15 @@ public abstract class ScreenshotRecorderMixin {
     @Inject(method = "saveScreenshotInner",
             at = @At(value = "INVOKE", target = "Ljava/util/concurrent/ExecutorService;execute(Ljava/lang/Runnable;)V"),
             locals = LocalCapture.CAPTURE_FAILHARD)
-    private static void captureLocals(File gameDirectory, String fileName, Framebuffer framebuffer, Consumer<Text> messageReceiver, CallbackInfo ci, NativeImage nativeImage, File file, File file2) {
-        // file2 represents the File that the screenshot was written to; the rest of the values here are irrelevant.
-        holdScreenshotFile(file2);
+    private static void captureAllLocals(File gameDirectory, String fileName, Framebuffer framebuffer, Consumer<Text> messageReceiver, CallbackInfo ci, NativeImage nativeImage, File file, File file2) {
+        // file2 represents the File that the screenshot was written to
+        // This and the messageReceiver are the only ones relevant here.
+        captureRelevantLocals(file2, messageReceiver);
     }
 
     @ModifyArg(method = "saveScreenshotInner",
                at = @At(value = "INVOKE", target = "Ljava/util/concurrent/ExecutorService;execute(Ljava/lang/Runnable;)V"))
-    private static Runnable sendToClipboard(Runnable screenshotSaver) {
+    private static Runnable injectSendToClipboard(Runnable screenshotSaver) {
         return () -> {
             screenshotSaver.run();
             sendHeldImageToClipboard();
